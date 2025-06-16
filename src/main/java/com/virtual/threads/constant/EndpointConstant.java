8fef77568ae09a9a3abb6a22b01e8902cc334e2a
JohnBrix <pomoy_johnbrix@icloud.com>
2025-06-16 16:32:31 +0800

    fixed sonarqube

--- src/main/java/com/virtual/threads/constant/EndpointConstant.java
+++ src/main/java/com/virtual/threads/constant/EndpointConstant.java
@@ -7,6 +7,11 @@ package com.virtual.threads.constant;
  * @version $Id: EndpointConstant.java, v 0.1 2025-06-04 10:20 PM John Brix Pomoy Exp $$
  */
 public class EndpointConstant {
+
+    private EndpointConstant(){
+
+    }
+
     public static final String BASE_ENDPOINT = "https://mdz9g.wiremockapi.cloud";
     public static final String KYC = "/kyc";
 
